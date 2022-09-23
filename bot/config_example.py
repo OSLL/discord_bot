4f2bedaf15d9de68b50b0cb21e6263952e931963
nevermore17 <nevermore.wmail@gmail.com>
2022-09-23 16:59:22 +0300

    Added config example file

---
+++ bot/config_example.py
@@ -0,0 +1,16 @@
+TOKEN = "Your discord bot token"
+GUILD = "Yuor discord server name"
+
+ADMIN_ROLE = "Admin tocken INT"
+
+PRIV_ROLES = ["Privelege tokens (INT)"]
+
+POST_ID = 1022855084993937448
+
+ROLES = {
+    "Emoji ID (String)" : "Role ID (INT)",
+}
+
+EXCROLES = ["RolesID (INT)"]
+
+MAX_ROLES_PER_USER = "MAX roles per user (INT)"
