10e256e116a9400ed5f5f00754d65e370767a4a6
nevermore17 <nevermore.wmail@gmail.com>
2022-09-23 19:10:15 +0300

    Added role remove handler

--- bot/main.py
+++ bot/main.py
@@ -23,13 +23,11 @@ class MyClient(discord.Client):
         if payload.message_id == config.POST_ID:
             channel = self.get_channel(payload.channel_id) 
             message = await channel.fetch_message(payload.message_id) 
-            member = payload.member
+            member = utils.get(message.guild.members, id=payload.user_id)
             
             try:
                 emoji = str(payload.emoji) 
-                print(emoji)
                 role = utils.get(message.guild.roles, id=config.ROLES[emoji])
-                print(role)
                 if(len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER):
                     await member.add_roles(role)
                     print('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))
@@ -42,9 +40,26 @@ class MyClient(discord.Client):
             except Exception as e:
                 print(repr(e))
     
+    async def on_raw_reaction_remove(self, payload):
+        channel = self.get_channel(payload.channel_id) # получаем объект канала
+        message = await channel.fetch_message(payload.message_id) # получаем объект сообщения
+        member = utils.get(message.guild.members, id=payload.user_id) # получаем объект пользователя который поставил реакцию
+ 
+        try:
+            emoji = str(payload.emoji) # эмоджик который выбрал юзер
+            role = utils.get(message.guild.roles, id=config.ROLES[emoji]) # объект выбранной роли (если есть)
+
+            await member.remove_roles(role)
+            print('[SUCCESS] Role {1.name} has been remove for user {0.display_name}'.format(member, role))
+ 
+        except KeyError as e:
+            print('[ERROR] KeyError, no role found for ' + emoji)
+        except Exception as e:
+            print(repr(e))
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 client = MyClient(intents=intents)
 client.run(token=config.TOKEN)
