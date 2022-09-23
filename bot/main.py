0085f35a37fd4da7d11da27aa4584a9a9c5f464e
nevermore17 <nevermore.wmail@gmail.com>
2022-09-23 20:29:18 +0300

    Added user delete and add role on join functions

--- bot/main.py
+++ bot/main.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 
 # print(type(my_token))
 
+delete_comand = "delete"
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Log: {0}'.format(self.user))
@@ -15,20 +17,42 @@ class MyClient(discord.Client):
                 break
 
     async def on_message(self, message):
-        # print(message)
-        print('Message "{0.author}": {0.content}'.format(message))
+        if message.channel.id == config.ADMIN_CHANEL_ID:
+            channel = self.get_channel(message.channel.id) 
+            message = await channel.fetch_message(message.id) 
+            member = utils.get(message.guild.members, id=message.author.id)
 
+            deleted_users = []
+
+            if(len([i for i in member.roles if i.id in config.PRIVROLES]) >= 1):
+                words = [i for i in message.content.split(' ') if i != '']
+                if( words[0] == delete_comand and len(words) >= 0): 
+                    for guild_member in message.guild.members:
+                        #Check that user is not Admin
+                        if(len([i for i in guild_member.roles if i.id == config.ADMIN_ROLE]) == 0):
+                            #Check user's roles
+                            if(len([i for i in guild_member.roles if i.name in words[1:]])):
+                                #Delete user
+                                deleted_users.append(guild_member.name)
+                                await guild_member.kick()
+            print("[INFO] Delete users: {0}".format(" ".join(deleted_users)))
+                
+
+
+    async def on_member_join(self, member):
+        guild = self.get_guild(member.guild.id)
+        await member.add_roles(guild.get_role(config.NEWUSERROLE_ID))
+        print("[INFO Added to {0} role {1}]".format(member, guild.get_role(config.NEWUSERROLE_ID)))
 
     async def on_raw_reaction_add(self, payload):
         if payload.message_id == config.POST_ID:
             channel = self.get_channel(payload.channel_id) 
             message = await channel.fetch_message(payload.message_id) 
             member = utils.get(message.guild.members, id=payload.user_id)
-            
             try:
                 emoji = str(payload.emoji) 
                 role = utils.get(message.guild.roles, id=config.ROLES[emoji])
-                if(len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER):
+                if(len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER or len([i for i in member.roles if i.id == config.ADMIN_ROLE]) == 1):
                     await member.add_roles(role)
                     print('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))
                 else:
