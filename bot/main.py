1a5cad9a72ed3b086afffadcf93048613e3635ac
nevermore17 <nevermore.wmail@gmail.com>
2022-09-30 15:14:03 +0300

    Added send log message after events

--- bot/main.py
+++ bot/main.py
@@ -3,15 +3,19 @@ from discord import utils
 
 import config
 
-# print(type(my_token))
 
 class MyClient(discord.Client):
+
+# Start bot log
     async def on_ready(self):
         print('Log: {0}'.format(self.user))
         for guild in client.guilds:
             if guild.name == config.GUILD:
                 break
+        await self.get_channel(config.ADMIN_CHANEL_ID).send("[INFO] BOT RUN")
 
+
+# Delete function
     async def on_message(self, message):
         if message.channel.id == config.ADMIN_CHANEL_ID:
             channel = self.get_channel(message.channel.id) 
@@ -31,15 +35,16 @@ class MyClient(discord.Client):
                                 #Delete user
                                 deleted_users.append(guild_member.name)
                                 await guild_member.kick()
-            print("[INFO] Delete users: {0}".format(" ".join(deleted_users)))
+                await channel.send("[INFO] Delete users: {0}".format(" ".join(deleted_users)))
                 
 
-
+# Get ROLE on member join
     async def on_member_join(self, member):
         guild = self.get_guild(member.guild.id)
         await member.add_roles(guild.get_role(config.NEWUSERROLE_ID))
-        print("[INFO Added to {0} role {1}]".format(member, guild.get_role(config.NEWUSERROLE_ID)))
+        await self.get_channel(config.ADMIN_CHANEL_ID).send("[INFO Added to {0} role {1}]".format(member, guild.get_role(config.NEWUSERROLE_ID)))
 
+# Get ROLE on memeber add reaction 
     async def on_raw_reaction_add(self, payload):
         if payload.message_id == config.POST_ID:
             channel = self.get_channel(payload.channel_id) 
@@ -50,30 +55,30 @@ class MyClient(discord.Client):
                 role = utils.get(message.guild.roles, id=config.ROLES[emoji])
                 if(len([i for i in member.roles if i.id not in config.EXCROLES]) <= config.MAX_ROLES_PER_USER or len([i for i in member.roles if i.id == config.ADMIN_ROLE]) == 1):
                     await member.add_roles(role)
-                    print('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))
+                    await self.get_channel(config.ADMIN_CHANEL_ID).send('[SUCCESS] User {0.display_name} has been granted with role {1.name}'.format(member, role))
                 else:
                     await message.remove_reaction(payload.emoji, member)
-                    print('[ERROR] Too many roles for user {0.display_name}'.format(member))
+                    await self.get_channel(config.ADMIN_CHANEL_ID).send('[ERROR] Too many roles for user {0.display_name}'.format(member))
             
             except KeyError as e:
-                print('[ERROR] KeyError, no role found for ' + emoji)
+                await self.get_channel(config.ADMIN_CHANEL_ID).send('[ERROR] KeyError, no role found for ' + emoji)
             except Exception as e:
                 print(repr(e))
-    
+
+ # Remove ROLE on memeber remove reaction    
     async def on_raw_reaction_remove(self, payload):
-        channel = self.get_channel(payload.channel_id) # получаем объект канала
-        message = await channel.fetch_message(payload.message_id) # получаем объект сообщения
-        member = utils.get(message.guild.members, id=payload.user_id) # получаем объект пользователя который поставил реакцию
- 
+        channel = self.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id) 
+        member = utils.get(message.guild.members, id=payload.user_id) 
         try:
-            emoji = str(payload.emoji) # эмоджик который выбрал юзер
-            role = utils.get(message.guild.roles, id=config.ROLES[emoji]) # объект выбранной роли (если есть)
+            emoji = str(payload.emoji)
+            role = utils.get(message.guild.roles, id=config.ROLES[emoji])
 
             await member.remove_roles(role)
-            print('[SUCCESS] Role {1.name} has been remove for user {0.display_name}'.format(member, role))
+            await self.get_channel(config.ADMIN_CHANEL_ID).send('[SUCCESS] Role {1.name} has been remove for user {0.display_name}'.format(member, role))
  
         except KeyError as e:
-            print('[ERROR] KeyError, no role found for ' + emoji)
+            await self.get_channel(config.ADMIN_CHANEL_ID).sendnt('[ERROR] KeyError, no role found for ' + emoji)
         except Exception as e:
             print(repr(e))
 
